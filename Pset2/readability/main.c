87ea2516f8e841a392517a12692f1d5da0a81c9b
moshe <moshe@kessler.tech>
2022-02-02 16:40:46 -0500

    remove testing text

--- Pset2/readability/main.c
+++ Pset2/readability/main.c
@@ -4,7 +4,7 @@
 
 int main(void)
 {
-    char *text = "It was a bright cold day in April, and the clocks were striking thirteen. Winston Smith, his chin nuzzled into his breast in an effort to escape the vile wind, slipped quickly through the glass doors of Victory Mansions, though not quickly enough to prevent a swirl of gritty dust from entering along with him."; // get_string("Text: ");
+    char *text = get_string("Text: ");
     struct Readability *textInput = makeText(text);
     int grade = getTextGrade(textInput);
     grade > 0 && grade <= 16 ? printf("Grade: %d\n", grade) : printf("%s\n", grade < 1 ? "Before Grade 1\n" : "Grade 16+\n");
